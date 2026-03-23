cd2b7f0e15fa7f03bfe4d18d30db1376d4303777
Spwindows <info@spwindowcleaning.com.au>
2026-03-23 21:34:53 +1100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,15 @@
-self.addEventListener("fetch", e => {});
+const CACHE='budgetflow-shared-fixed-v1';
+const ASSETS=['./','./index.html','./manifest.json'];
+
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate',e=>{
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch',e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
